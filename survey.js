e13886a502355341b191f07452389019834975ec
Marcela Ang <marcela.ang@gmail.com>
2022-11-11 11:34:19 -0500

    M1W4 - Profile generator using readline, eslint run

--- survey.js
+++ survey.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What\'s your name? Nicknames are also acceptable :\) ' , (answerName) => {
+rl.question('What\'s your name? Nicknames are also acceptable :) ' , (answerName) => {
   rl.question('What\'s an activity you like doing? ', (answerHobby) => {
     rl.question('What do you listen to while doing that? ', (answerListen) => {
       rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answerMeal) => {
@@ -19,42 +19,5 @@ rl.question('What\'s your name? Nicknames are also acceptable :\) ' , (answerNam
         });
       });
     });
-  });        
+  });
 });
-
-// rl.question('What\'s an activity you like doing?', (answerHobby) => {
-//   console.log(`I enjoy ${answerHobby}.`);
-
-//   rl.close();
-// });
-
-// rl.question('What do you listen to while doing that?', (answerListen) => {
-//   console.log(`While  ${answerHobby}, I like to listen to ${answerListen}.`);
-
-//   rl.close();
-// });
-
-// rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answerMeal) => {
-//   console.log(`${answerMeal} is my favourite meal.`);
-
-//   rl.close();
-// });
-
-// rl.question('What\'s your favourite thing to eat for that meal?', (answerFood) => {
-//   console.log(`For ${answerMeal} I love having ${answerFood}.`);
-
-//   rl.close();
-// });
-
-// rl.question('Which sport is your absolute favourite?', (answerSport) => {
-//   console.log(`My favourite sport is ${answerSport}.`);
-
-//   rl.close();
-// });
-
-
-// rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answerSP) => {
-//   console.log(`I think my superpower is ${answerSP}!`);
-
-//   rl.close();
-// });
